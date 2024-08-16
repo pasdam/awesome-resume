09611729a584b6ec3c469aae1c6b7230ce570d78
pasdam <pasdam@users.noreply.github.com>
2024-08-16 13:40:32 +0800

    feat: re-generated static files

---
+++ _next/static/vxCV7mcHnFPPI1MMvKuTb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b1c6985f1335c9c8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
